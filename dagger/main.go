3b574d8b7a4aca1117f5238e497b117c717e15f4
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2024-08-08 00:34:02 +0200

    add reaction
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- dagger/main.go
+++ dagger/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	"github.com/google/go-github/v63/github"
+	"golang.org/x/oauth2"
 )
 
 type DaggerGithubComment struct {
@@ -38,6 +39,28 @@ func (m *DaggerGithubComment) Process(
 	}
 
 	fmt.Println("Event: ", event.Issue.GetID())
+
+	githubToken, err := m.GithubToken.Plaintext(ctx)
+	if err != nil {
+		return err
+	}
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	_, _, err = client.Reactions.CreateIssueCommentReaction(
+		ctx,
+		event.GetRepo().GetOwner().GetName(),
+		event.GetRepo().GetName(),
+		event.GetComment().GetID(),
+		"+1",
+	)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
